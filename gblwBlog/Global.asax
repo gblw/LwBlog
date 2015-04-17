4f3f0dc113298cddf63a8bf9c46a05b6d49c7e2c
gblw <iamgblw@163.com>
2015-04-17 23:23:44 +0800

    11

---
+++ gblwBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gblwBlog.MvcApplication" Language="C#" %>
---
+++ gblwBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gblwBlog.Startup))]
+namespace gblwBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
