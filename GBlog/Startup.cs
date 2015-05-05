5470d4bd5d73df750024e011699280e75a1ffce5
gblw <iamgblw@163.com>
2015-05-05 21:59:11 +0800

    1

---
+++ GBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GBlog.Startup))]
+namespace GBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
